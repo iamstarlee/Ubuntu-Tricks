50339828fc4b77acdfd5a706bc13767bcba3db0e
Xinxin Li <44799727+iamstarlee@users.noreply.github.com>
2024-08-20 11:16:05 +0800

    Create C2f.py

---
+++ C2f.py
@@ -0,0 +1,27 @@
+class C2f(nn.Module):
+    """CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        # y = list(self.cv1(x).chunk(2, 1))
+        # y.extend(m(y[-1]) for m in self.m)
+        # return self.cv2(torch.cat(y, 1))
+        x = self.cv1(x)
+        x1, x2 = torch.chunk(x, 2, dim = 1)
+        for m in self.m:
+            x2 = m(x2)
+        concatenation = torch.cat((x1, x2), dim = 1)
+        return self.cv2(concatenation)
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
